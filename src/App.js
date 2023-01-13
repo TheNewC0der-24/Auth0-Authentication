af891625681af5ccb421b16526db163efcfae9cb
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-01-13 23:41:40 +0530

    Create route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+// Components
+import Header from './Components/Header/Header';
+import Login from './Components/Login/Login';
+
+// Pages
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Pricing from './Pages/Pricing/Pricing';
+import Contact from './Pages/ContactUs/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
