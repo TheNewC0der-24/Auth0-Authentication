4e6f2b0fbcbd0984cb1f33d401f5832422122308
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-07-16 00:28:58 +0530

    Add redirect uri

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const uri = "http://localhost:3000/home";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
+    redirectUri={uri}
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
