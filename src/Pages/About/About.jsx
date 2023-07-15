30b3e59b951290753a72668eb7d0b1d3fd42a29c
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-07-16 02:57:32 +0530

    Create about page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,8 +1,47 @@
 import React from 'react';
+import { Box, Card, CardContent, Container, Grid, Typography } from '@mui/material';
 
 const About = () => {
     return (
-        <div>About</div>
+        <Container maxWidth="lg">
+            <Box sx={{ mt: 5 }}>
+                <Typography variant="h4" align='center'>
+                    Lorem ipsum dolor sit amet.
+                </Typography>
+                <Typography variant="h6" align='center'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, aliquam!
+                </Typography>
+                <Grid container spacing={2} sx={{ mt: 5 }}>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Card sx={{ bgcolor: "#388BFF" }}>
+                            <CardContent>
+                                <Typography variant="h5" align='center'>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, nihil unde ducimus magni tempora optio iure quibusdam ea nemo atque iusto maiores deserunt! Corporis, in.
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Card sx={{ bgcolor: "#FEC57B" }}>
+                            <CardContent>
+                                <Typography variant="h5" align='center'>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, nihil unde ducimus magni tempora optio iure quibusdam ea nemo atque iusto maiores deserunt! Corporis, in.
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Card sx={{ bgcolor: "#B3DF72" }}>
+                            <CardContent>
+                                <Typography variant="h5" align='center'>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, nihil unde ducimus magni tempora optio iure quibusdam ea nemo atque iusto maiores deserunt! Corporis, in.
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Container>
     )
 }
 
