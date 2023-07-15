f3fd841fcac6cc4a6650277c56ee805dfc75388c
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-07-16 00:29:39 +0530

    Handling redirect on the basis of authenticated status

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Navigate
 } from "react-router-dom";
+
+import { useAuth0 } from '@auth0/auth0-react';
 
 // Components
 import Header from './Components/Header/Header';
@@ -16,14 +19,17 @@ import About from './Pages/About/About';
 import Contact from './Pages/ContactUs/Contact';
 
 function App() {
+
+  const { isAuthenticated } = useAuth0();
+
   return (
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Welcome />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/home" element={isAuthenticated ? <Home /> : <Navigate to="/" replace />} />
+        <Route path="/about" element={isAuthenticated ? <About /> : <Navigate to="/" replace />} />
+        <Route path="/contact" element={isAuthenticated ? <Contact /> : <Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
