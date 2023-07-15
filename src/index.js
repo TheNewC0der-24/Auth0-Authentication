ae5480e329a019c56fc1f0e05a0161b78bf96760
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-07-16 02:02:48 +0530

    Change uri

--- src/index.js
+++ src/index.js
@@ -6,16 +6,15 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-const uri = "http://localhost:3000/home";
+const uri = "https://auth0-authentication.vercel.app/home";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
-    redirectUri={uri}
     authorizationParams={{
-      redirect_uri: window.location.origin
+      redirect_uri: uri,
     }}
   >
     <App />
