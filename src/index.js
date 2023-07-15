aca85853bda357cb112cc19224493ee7cd2e0fc0
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-07-15 23:39:37 +0530

    Remove console

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-console.log(domain);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Auth0Provider
