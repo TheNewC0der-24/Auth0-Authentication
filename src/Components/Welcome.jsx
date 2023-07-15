ecd369248b350651f59b4b8a376f6f864de79007
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-07-16 01:39:49 +0530

    Create welcome page

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 
+import { Box, Typography, Button } from '@mui/material';
+import { useAuth0 } from '@auth0/auth0-react';
+
 const Welcome = () => {
+
+    const { loginWithRedirect } = useAuth0();
+
+
     return (
-        <div>
-            Welcome
-        </div>
+        <Box sx={{ mt: 5, mb: 5 }}>
+            <Typography variant="h4" align="center">Welcome to the React application.</Typography>
+            <Typography variant="h6" align="center">Start your journey with us.</Typography>
+
+            <Box sx={{ mt: 5, mb: 5, display: "flex", justifyContent: "center" }}>
+                <Button
+                    color="primary"
+                    variant='contained'
+                    onClick={() => loginWithRedirect()}
+                >
+                    Login
+                </Button>
+            </Box>
+        </Box>
     )
 }
 
